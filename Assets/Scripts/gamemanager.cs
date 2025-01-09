0d623f99f9e3324224072864118e96316b5b97e0
Eddpoxy <edward.lundh@elev.ga.lbs.se>
2025-01-09 13:57:19 +0100

    Lägg till / ta bort mat funktion i GameManager

--- Assets/Scripts/gamemanager.cs
+++ Assets/Scripts/gamemanager.cs
@@ -6,23 +6,72 @@ public class GameManager : MonoBehaviour
 {
     public int Food;
     public List<GameObject> FoodSprites;
+    public Transform FoodSpawnPosition;
+
+    // List to keep track of all instantiated food objects
+    private List<GameObject> spawnedFoodObjects = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      
+      
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    } 
-    void AddFood(int Amount)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(RemoveFood(10));
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(AddFood(10));
+        }
+    }
+
+    public IEnumerator AddFood(int Amount)
     {
         Food += Amount;
+        for (int i = 0; i <= Amount; i++)
+        {
+            // Select a random GameObject from the FoodSprites list
+            int randomIndex = Random.Range(0, FoodSprites.Count);
+            GameObject randomFood = FoodSprites[randomIndex];
+
+            // Instantiate the food and add it to the spawnedFoodObjects list
+            GameObject spawnedFood = Instantiate(randomFood, FoodSpawnPosition.transform.position, Quaternion.identity);
+            spawnedFoodObjects.Add(spawnedFood);
+
+            yield return new WaitForSeconds(0.25f);
+        }
     }
-    void RemoveFood(int Amount)
+
+    public IEnumerator RemoveFood(int Amount)
     {
-        Food -= Amount;
+        for (int i = 0; i < Amount; i++)
+        {
+            if (spawnedFoodObjects.Count > 0)
+            {
+                // Remove the oldest (first) food object in the list
+                GameObject foodToRemove = spawnedFoodObjects[0];
+                spawnedFoodObjects.RemoveAt(0);
+
+                // Destroy the food GameObject
+                Destroy(foodToRemove);
+
+                // Decrease the food counter
+                Food--;
+
+                yield return new WaitForSeconds(0.25f); // Delay between removing each food
+            }
+            else
+            {
+                // If no food is left, break out of the loop
+                break;
+            }
+        }
     }
 }
