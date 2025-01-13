fa4ae8e598ae48be6fd4068cefa1e56563a29953
Duckman5 <christoffer.lindberg@elev.ga.lbs.se>
2025-01-13 10:33:34 +0100

    game over

--- Assets/Scripts/Game over scene.cs
+++ Assets/Scripts/Game over scene.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Gameoverscene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("Edwards Scene"); // Byt ut "GameScene" mot namnet på din spelscen
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Debug.Log("Quit Game");
+        Application.Quit(); // Avslutar spelet (fungerar endast i byggd version)
     }
 }
+
+
+
+
+
+
+
