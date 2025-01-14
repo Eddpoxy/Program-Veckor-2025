cab29935a6224243e663f97651bd1f5f776a422c
Eddpoxy <edward.lundh@elev.ga.lbs.se>
2025-01-14 13:38:18 +0100

    Text sound

--- Assets/Scripts/Npc Variants/NPCVariant1.cs
+++ Assets/Scripts/Npc Variants/NPCVariant1.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class NPCVariant1 : NPCS
 {
 
-    // Overriding Start to modify dialogue specific to NPCVariant1
+
     protected override void Start()
     {
-        base.Start(); // Call base class Start to ensure EntranceTransform is set
+        base.Start();
 
         string previousChoice = GameManager.Instance.GetChoice(npcID);
         if (previousChoice == "No")
@@ -29,29 +29,20 @@ public class NPCVariant1 : NPCS
         }
     }
 
-    // If you want to modify walk behavior, override WalkIntoScene here
-    protected override void WalkIntoScene()
-    {
-        base.WalkIntoScene();
-    }
-
-    // Modified ShowDialogueChoices to show text bubble when NPC remembers previous choices
     protected override IEnumerator ShowDialogueChoices()
     {
         string previousChoice = GameManager.Instance.GetChoice(npcID);
 
         if (previousChoice == "No")
         {
-            // Show the custom dialogue when the NPC remembers a previous choice
-            ShowTextBubble(Dialogue[0]);  // Show text bubble with the dialogue about "No"
-            Debug.Log(Dialogue[0]); // Log the custom dialogue for this case
-            GameManager.Instance.StartCoroutine(GameManager.Instance.RemoveFood(10)); // Take 10 food automatically
-            yield return new WaitForSeconds(2);  // Wait for the text to be visible
-            ExitScene();  // NPC leaves the scene after a set time
-            yield break;  // Skip further dialogue choices and exit
+            ShowTextBubble(Dialogue[0]);
+            Debug.Log(Dialogue[0]);
+            GameManager.Instance.StartCoroutine(GameManager.Instance.RemoveFood(10));
+            yield return new WaitForSeconds(2);
+            ExitScene();
+            yield break;
         }
 
-        // Otherwise, fall back to the default choice behavior if no previous choice or a different choice
         yield return base.ShowDialogueChoices();
     }
 
