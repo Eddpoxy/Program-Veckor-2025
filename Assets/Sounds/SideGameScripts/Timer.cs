98826b8e9e4b06d119f0067eb02f0a9ef6f63f3f
SebastianEklund1 <sebastian.eklund2@elev.ga.lbs.se>
2025-01-17 12:48:56 +0100

    SideGame helt klart

--- Assets/Sounds/SideGameScripts/Timer.cs
+++ Assets/Sounds/SideGameScripts/Timer.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI Time;
-    [SerializeField] private int TimeInSeconds = 90;
+    [SerializeField] private int TimeInSeconds = 60;
+
+    private SpriteRenderer spriten;
+    [SerializeField] private GameObject Crack;
+    [SerializeField] private GameObject Player;
     private int currentTime;
 
     private void Start()
     {
         currentTime = TimeInSeconds;
 
+        GetComponent<SpriteRenderer>();
 
         StartCoroutine(Countdown());
     }
@@ -29,5 +35,9 @@ public class Timer : MonoBehaviour
         }
 
         Time.text = "Time's up!";
+
+        Instantiate<GameObject>(Crack, Player.transform.position, Quaternion.identity);
+
+        SceneManager.LoadScene("first start scene");
     }
 }
